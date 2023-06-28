80bce5ed309d6e3318455e67c38484cc206a4dd3
David Huertas <davidhuertasortiz@gmail.com>
2023-06-29 00:34:21 +0200

    add: logo components

---
+++ src/img/DiscordFooterLogo.tsx
@@ -0,0 +1,26 @@
+const DiscordFooterLogo = () => (
+  <svg xmlns='http://www.w3.org/2000/svg' width={62} height={48} fill='none'>
+    <path
+      stroke='#F5835E'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      strokeWidth={1.5}
+      d='M11.487 36.458c14.98 7.404 23.968 7.404 38.948 0M41.447 40.9l2.995 5.923S56.94 42.89 60.92 36.458c0-2.961 1.588-24.126-8.988-31.094C47.438 2.403 39.95.922 39.95.922l-2.996 5.923H30.96'
+    />
+    <path
+      stroke='#F5835E'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      strokeWidth={1.5}
+      d='m20.559 40.9-2.996 5.923S5.066 42.89 1.085 36.458c0-2.961-1.588-24.126 8.988-31.094C14.567 2.403 22.057.922 22.057.922l2.996 5.923h5.991'
+    />
+    <path
+      stroke='#F5835E'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      strokeWidth={1.5}
+      d='M20.475 30.536c-2.481 0-4.494-2.65-4.494-5.923 0-3.272 2.013-5.923 4.494-5.923 2.48 0 4.494 2.65 4.494 5.923 0 3.272-2.014 5.923-4.494 5.923Zm20.972 0c-2.481 0-4.494-2.65-4.494-5.923 0-3.272 2.013-5.923 4.494-5.923 2.48 0 4.494 2.65 4.494 5.923 0 3.272-2.014 5.923-4.494 5.923Z'
+    />
+  </svg>
+)
+export default DiscordFooterLogo
