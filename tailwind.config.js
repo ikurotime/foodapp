debcf2fbda26978c59d83a6e2cffa6e2012211ac
David Huertas <davidhuertasortiz@gmail.com>
2023-07-06 00:37:10 +0200

    add: next & tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
         'login-background': 'url("/food_login.png")'
+      },
+      colors: {
+        cream: '#EFE4DA'
       }
     }
   },
